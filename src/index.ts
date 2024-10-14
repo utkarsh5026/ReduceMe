6b1c31f0f3081d2b426ff246a3c97ebadcafc02c
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-10-15 01:07:56 +0530

    added an index.ts file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { createSlice } from "./slice";
+import { createAction } from "./actions";
+import { Store, combineReducers, dispatch } from "./store";
+
+export { createSlice, createAction, Store, combineReducers, dispatch };
